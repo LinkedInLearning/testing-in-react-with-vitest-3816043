8e15a9b81e0326c97ffbe719c060b9fd8c9b0dda
Toxic254 <cyrus_g@outlook.com>
2024-03-11 20:16:43 +0300

    Wrote our first unit test.

---
+++ tests/App.test.jsx
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '@testing-library/react';
+
+import App from '../src/App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    // ARRANGE
+    render(<App />);
+
+    // ACT
+
+    // ASSERT
+    expect(screen.getByRole('heading', {
+      level: 1,
+    })).toHaveTextContent('Hello World');
+  })
+});
