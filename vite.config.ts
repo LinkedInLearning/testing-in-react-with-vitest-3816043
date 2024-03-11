f72f8981328ef52091c67069a02eb86fc94e5510
Toxic254 <cyrus_g@outlook.com>
2024-03-11 17:42:55 +0300

    Added vitest and fixed test error in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    pool: 'forks',
+  },
+}))
