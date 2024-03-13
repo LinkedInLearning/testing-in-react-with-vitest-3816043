49d7f20b73cd8b621d348ef197982bd9c3acfff2
Toxic254 <cyrus_g@outlook.com>
2024-03-13 15:27:50 +0300

    Text change

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ function NotFound() {
   return (
     <div>
       <h1>
-        Sorry the page you're looking for cannot be found, head{' '}
+        Sorry the page you are looking for cannot be found, head{' '}
         <Link to="/">Home</Link> and try again
       </h1>
     </div>
