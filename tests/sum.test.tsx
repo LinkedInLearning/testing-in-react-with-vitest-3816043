c450aae069db3587fe5ab3e80dbd188934ba48f2
Toxic254 <cyrus_g@outlook.com>
2024-03-25 19:16:58 +0300

    Written our first unit test

---
+++ tests/sum.test.tsx
@@ -0,0 +1,11 @@
+import { expect, it } from 'vitest';
+
+import { sum, multiply } from '../src/sum';
+
+it('Should add two numbers', () => {
+  expect(sum(12, 12)).toBe(24);
+});
+
+it('Should multiply two numbers', () => {
+  expect(multiply(12, 12)).toBe(144);
+});
