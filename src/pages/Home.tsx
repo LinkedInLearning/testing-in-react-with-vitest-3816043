617d1d90eb940ab15919fc579b5dc680e29ceeff
Toxic254 <cyrus_g@outlook.com>
2024-03-25 14:07:27 +0300

    Added our fruit search feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,48 @@
+import { useState } from 'react';
+import { Fruits } from '../components/Fruits';
+
 function Home() {
+  const [fruit, setFruit] = useState<any>(undefined);
+  const [fruitName, setFruitName] = useState<string>('');
+
+  const fetchFruitData = async () => {
+    const fruit = await Fruits(fruitName);
+
+    if (!fruit) {
+      return setFruit(undefined);
+    }
+
+    setFruit(fruit);
+
+    return fruit;
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
+      <div>
+        <input
+          type="text"
+          value={fruitName}
+          onChange={(e) => setFruitName(e.target.value)}
+        />
+        <button onClick={fetchFruitData}>Find Fruit Data</button>
+      </div>
+      <div>
+        {fruit && (
+          <div>
+            <h2>{fruit.name}</h2>
+            <p>Family: {fruit.family}</p>
+            <p>Genus: {fruit.genus}</p>
+            <p>Order: {fruit.order}</p>
+            <p>Calories: {fruit.nutritions.calories}</p>
+            <p>Fat: {fruit.nutritions.fat}</p>
+            <p>Sugar: {fruit.nutritions.sugar}</p>
+            <p>Carbohydrates: {fruit.nutritions.carbohydrates}</p>
+            <p>Protein: {fruit.nutritions.protein}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
