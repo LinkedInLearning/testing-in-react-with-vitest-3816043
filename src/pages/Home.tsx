02a60d91f9057a3438898c8cbbc0025462261829
Toxic254 <cyrus_g@outlook.com>
2024-06-17 16:21:54 +0300

    chore: Add NavBar component and update Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import NavBar from '../components/Navbar';
 import FruitSearch from '../components/FruitSearch';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-center p-3">Hello World</h1>
-      <FruitSearch />
-    </div>
+    <>
+      <NavBar />
+      <div>
+        <h1 className="text-center p-3">Hello World</h1>
+        <FruitSearch />
+      </div>
+    </>
   );
 };
 
