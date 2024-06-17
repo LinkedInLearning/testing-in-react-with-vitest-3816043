6eb0e3254603e2623272a3adf09def5bbc88135a
Toxic254 <cyrus_g@outlook.com>
2024-06-17 15:41:51 +0300

    chore: Add Navbar component for navigation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Testing in React</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-end">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
