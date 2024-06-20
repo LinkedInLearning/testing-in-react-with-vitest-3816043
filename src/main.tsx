69ec2f84bec7f242a1861cda0021c253c9f723c8
Toxic254 <cyrus_g@outlook.com>
2024-06-20 17:51:27 +0300

    feat: Wrapped our application with our Provider and passed our store.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import { WrappedApp } from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WrappedApp />
+    <Provider store={store}>
+      <WrappedApp />
+    </Provider>
   </React.StrictMode>
 );
