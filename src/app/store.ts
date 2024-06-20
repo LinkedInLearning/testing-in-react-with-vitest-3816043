505253fdb07ab7417be1944f2869afed632b294c
Toxic254 <cyrus_g@outlook.com>
2024-06-20 17:49:06 +0300

    chore: Added store to app folder

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import fruitsReducer from '../features/fruits/fruitSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    fruits: fruitsReducer
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
