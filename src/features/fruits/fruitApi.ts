700e6b2bd391bf5a65268a7f1c41ec7aedce65da
Toxic254 <cyrus_g@outlook.com>
2024-06-21 11:28:08 +0300

    chore: Added RTK-Query API

--- src/features/fruits/fruitApi.ts
+++ src/features/fruits/fruitApi.ts
@@ -1,11 +1,16 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { loadFruit } from '../../components/Fruits';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Fruit } from '../../components/Fruits';
 
-// Async thunk for fetching fruit
-export const fetchFruit = createAsyncThunk(
-  'fruits/fetchFruit',
-  async (name: string) => {
-    const fruit = await loadFruit(name);
-    return fruit;
-  }
-);
+const baseUrl = 'https://www.fruityvice.com/api/fruit/'; // Mock API base URL
+
+export const fruitApi = createApi({
+  reducerPath: 'fruitApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getFruitByName: builder.query<Fruit, string>({
+      query: (name) => `/${name}`,
+    }),
+  }),
+});
+
+export const { useGetFruitByNameQuery } = fruitApi;
