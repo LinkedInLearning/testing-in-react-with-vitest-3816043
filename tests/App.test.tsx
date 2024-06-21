7784b0e948e0bd5e6c3d781ce1c775657eb8f113
Toxic254 <cyrus_g@outlook.com>
2024-06-21 14:29:06 +0300

    chore: Removed unused libs

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
 
