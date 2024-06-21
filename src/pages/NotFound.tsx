674bb34282145f8d5c9c6bc7a95cdb62dabc3acd
Toxic254 <cyrus_g@outlook.com>
2024-06-21 11:39:40 +0300

    Added NavBar to Not Found Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom';
+import NavBar from '../components/Navbar';
 
 function NotFound() {
   return (
-    <div>
-      <h1>
-        Sorry the page you are looking for cannot be found, head{' '}
-        <Link to="/">Home</Link> and try again
-      </h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container p-5 mt-4 justify-content-center text-center flex">
+        <h1>
+          Sorry the page you are looking for cannot be found, head{' '}
+          <Link to="/">Home</Link> and try again
+        </h1>
+      </div>
+    </>
   );
 }
 
