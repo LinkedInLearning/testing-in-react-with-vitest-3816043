8b9f2896921356ce03ebfd737753d24b7963feca
Toxic254 <cyrus_g@outlook.com>
2024-06-21 11:39:22 +0300

    chore: Used Link to anchor our links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Testing in React</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/" className="text-decoration-none">
+            Testing in React
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-end">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Link to="/" className="text-decoration-none">
+              Home
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
