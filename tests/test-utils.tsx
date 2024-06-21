7883c960d9370aca409da3f2e0fe9d23be1d2892
Toxic254 <cyrus_g@outlook.com>
2024-06-21 11:04:12 +0300

    chore: Add test-utils module for rendering components with providers

---
+++ tests/test-utils.tsx
@@ -0,0 +1,36 @@
+// src/utils/test-utils.tsx
+import React, { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux'
+import { render, RenderOptions } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+import fruitsReducer from '../src/features/fruits/fruitSlice';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: {};
+  store?: ReturnType<typeof configureStore>;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: { fruits: fruitsReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  setupListeners(store.dispatch);
+
+  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+    return (
+      <Provider store= { store } > { children } </Provider>
+    );
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
