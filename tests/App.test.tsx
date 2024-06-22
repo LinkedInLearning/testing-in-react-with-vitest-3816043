99ff1f7e26857dd2321b946e9985821019b73b38
Toxic254 <cyrus_g@outlook.com>
2024-06-22 04:15:30 +0300

    chore: Base refactor

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,9 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import { MemoryRouter } from 'react-router-dom';
-
-import { WrappedApp, App } from '../src/App';
+import { WrappedApp } from '../src/App';
 
 describe('App', () => {
   it('Renders hello world', () => {
@@ -18,25 +16,5 @@ describe('App', () => {
         level: 1,
       })
     ).toHaveTextContent('Hello World');
-  });
-
-  it('Renders not found if path is invalid', () => {
-    // ARRANGE
-    render(
-      <MemoryRouter initialEntries={['/j']}>
-        <App />
-      </MemoryRouter>
-    );
-
-    // ACT
-
-    // ASSERT
-    expect(
-      screen.getByRole('heading', {
-        level: 1,
-      })
-    ).toHaveTextContent(
-      'Sorry the page you are looking for cannot be found, head Home and try again'
-    );
   });
 });
